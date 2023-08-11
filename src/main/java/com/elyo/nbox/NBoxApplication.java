03c8d66ae9a51f0865f36b363c0167eb35716a61
eri <44278562+elyo9381@users.noreply.github.com>
2023-08-11 11:26:39 +0900

    Initial commit

---
+++ src/main/java/com/elyo/nbox/NBoxApplication.java
@@ -0,0 +1,13 @@
+package com.elyo.nbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elyo/nbox/NBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elyo.nbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
