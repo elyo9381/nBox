7700e3ad5c9e175ddc92051445d1b75a5f92da2a
eri <44278562+elyo9381@users.noreply.github.com>
2023-08-15 13:46:46 +0900

    feat 간단한 파일 업로드 api

--- src/main/java/com/elyo/nbox/file/controller/FileController.java
+++ src/main/java/com/elyo/nbox/file/controller/FileController.java
@@ -1,16 +1,14 @@
 package com.elyo.nbox.file.controller;
 
+import com.elyo.nbox.file.controller.dto.FileResponse;
 import com.elyo.nbox.file.service.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping("/")
@@ -21,15 +19,6 @@ public class FileController {
 
     @PostMapping("upload")
     public FileResponse upload(MultipartHttpServletRequest request) throws IOException {
-
-        List<MultipartFile> files = request.getFiles("files");
-
-        int size = files.size();
-        for (int i = 0; i < size; i++) {
-            MultipartFile file = files.get(i);
-            file.transferTo(new File("testfile"+i));
-        }
-
-        return null;
+        return fileService.upload(request);
     }
 }
--- src/main/java/com/elyo/nbox/file/controller/FileRequest.java
+++
@@ -1,4 +0,0 @@
-package com.elyo.nbox.file.controller;
-
-public class FileRequest {
-}
